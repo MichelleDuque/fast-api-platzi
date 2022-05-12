fc094bcb4d303ca92d09118f204b2b16257fe710
MichelleDuque <michelled_1994@hotmail.com>
2022-05-11 19:50:00 -0500

    remove_duplicate

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Location(BaseModel):
     state: str
     country: str
 
-class Person(BaseModel): 
+class personBase(BaseModel):
     first_name: str = Field(
         ..., 
         min_length=1,
@@ -47,7 +47,14 @@ class Person(BaseModel):
     )
     hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
     is_married: Optional[bool] = Field(default=None, example=False)
-    password: str = Field(..., min_length=8)
+    
+
+class Person(personBase): 
+    password: str = Field(
+        ..., 
+        min_length=8,
+        example = "soymiguel"
+        )
 
     # class Config: 
     #     schema_extra = {
@@ -60,27 +67,9 @@ class Person(BaseModel):
     #         }
     #     }
 
-class PersonOut(BaseModel): 
-    first_name: str = Field(
-        ..., 
-        min_length=1,
-        max_length=50,
-        example="Miguel"
-        )
-    last_name: str = Field(
-        ..., 
-        min_length=1,
-        max_length=50,
-        example="Torres"
-        )
-    age: int = Field(
-        ...,
-        gt=0,
-        le=115,
-        example=25
-    )
-    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
-    is_married: Optional[bool] = Field(default=None, example=False)
+class PersonOut(personBase): 
+    pass
+
 
 @app.get("/")
 def home(): 
