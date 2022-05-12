50d9290bc6ba8be8164fdca33b952b3c83e0db89
MichelleDuque <michelled_1994@hotmail.com>
2022-05-11 19:34:11 -0500

    response_model

--- main.py
+++ main.py
@@ -1,168 +1,145 @@
-# \venv\Scripts\activate
-
-#python
-from dataclasses import field
-import imp
+#Python
 from typing import Optional
 from enum import Enum
 
-
 #Pydantic
-from pydantic import BaseModel, Field, EmailStr, PaymentCardNumber, HttpUrl
+from pydantic import BaseModel
+from pydantic import Field
 
-#FASTAPI
-from fastapi import FastAPI, Query
+#FastAPI
+from fastapi import FastAPI
 from fastapi import Body, Query, Path
 
 app = FastAPI()
 
-#Models
+# Models
 
-class HairColor(Enum):
+class HairColor(Enum): 
     white = "white"
     brown = "brown"
     black = "black"
     blonde = "blonde"
     red = "red"
 
-class Location(BaseModel):
-    city: str = Field(
-        ..., 
-        min_length= 1,
-        max_length= 115,
-        example = "Barranquilla"
-        )
-    state: str = Field(
-        ..., 
-        min_length= 1,
-        max_length= 115,
-        example = "Atlantico"
-        )
-    country: str = Field(
-        ..., 
-        min_length= 1,
-        max_length= 115,
-        example = "Colombia"
-        )
+class Location(BaseModel): 
+    city: str
+    state: str
+    country: str
 
-class Person(BaseModel):
+class Person(BaseModel): 
     first_name: str = Field(
         ..., 
-        min_length= 1,
-        max_length= 50,
-        example = "Michelle"
+        min_length=1,
+        max_length=50,
+        example="Miguel"
         )
     last_name: str = Field(
         ..., 
-        min_length= 1,
-        max_length= 50,
-        example = "Duque"
+        min_length=1,
+        max_length=50,
+        example="Torres"
         )
     age: int = Field(
         ...,
         gt=0,
         le=115,
-        example = 27
+        example=25
     )
-    hair_color: Optional[HairColor] = Field(
-        default=None,
-        example = "black"
-    )
-    is_married: Optional[bool] = Field(
-        default= None,
-        example = False
-    )
-    email: EmailStr = Field(
-        ...,
-        min_length=1,
-        example = "michelle@gmail.com"
-    )
-    payment_card_number: PaymentCardNumber = Field(
-        ...,
-        min_length=1,
-        example = 53534523
-    )
-    httpurl: Optional[HttpUrl] = field(
-        default=None,
-        example = "https://twitter.com/home"
-    )
+    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
+    is_married: Optional[bool] = Field(default=None, example=False)
+    password: str = Field(..., min_length=8)
 
-    # class Config:
+    # class Config: 
     #     schema_extra = {
-    #         "Facundo": {
+    #         "example": {
     #             "first_name": "Facundo",
-    #             "last_name": "Garcia Martoni",
-    #             "age": 21,
+    #             "last_name": "García Martoni",
+    #             "age": 21, 
     #             "hair_color": "blonde",
-    #             "is_married": False,
-    #             "email": "facundo@gmail.com",
-    #             "payment_card_number": 43244564563453,
-    #             "httpurl": "https://platzi.com/"
+    #             "is_married": False
     #         }
     #     }
 
+class PersonOut(BaseModel): 
+    first_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50,
+        example="Miguel"
+        )
+    last_name: str = Field(
+        ..., 
+        min_length=1,
+        max_length=50,
+        example="Torres"
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example=25
+    )
+    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
+    is_married: Optional[bool] = Field(default=None, example=False)
 
 @app.get("/")
-def home():
+def home(): 
     return {"Hello": "World"}
 
-@app.post("/person/new")
-def create_person(person: Person = Body(...)):
+# Request and Response Body
+
+@app.post("/person/new", response_model=PersonOut)
+def create_person(person: Person = Body(...)): 
     return person
 
 # Validaciones: Query Parameters
 
-
 @app.get("/person/detail")
 def show_person(
     name: Optional[str] = Query(
-        None, 
+        None,
         min_length=1, 
         max_length=50,
-        title="Person name",
+        title="Person Name",
         description="This is the person name. It's between 1 and 50 characters",
-        example= "Rocío"
+        example="Rocío"
         ),
     age: str = Query(
         ...,
-        title="Person age",
-        description="This is the person age. It's required.",
-        example= 25
+        title="Person Age",
+        description="This is the person age. It's required",
+        example=25
         )
-):
-    return{name: age}
+): 
+    return {name: age}
 
-
-#Validations: Path Parameters
+# Validaciones: Path Parameters
 
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
         ..., 
         gt=0,
-        title="Person ID",
-        description="This is the person ID. It's required and must be greater than 0.",
-        example= 123
+        example=123
         )
-):
-    return{person_id: "It exits!"}
+): 
+    return {person_id: "It exists!"}
 
-#Validaciones: Request Body
+# Validaciones: Request Body
 
-@app.put("/person({person_id}")
+@app.put("/person/{person_id}")
 def update_person(
     person_id: int = Path(
         ...,
         title="Person ID",
         description="This is the person ID",
         gt=0,
-        example= 123
+        example=123
     ),
-    person: Person = Body(
-        ...
-        ),
-    location: Location = Body(...)
-):
-    results = person.dict()
-    results.update(location.dict())
-    return results
+    person: Person = Body(...),
+    #location: Location = Body(...)
+): 
+    #results = person.dict()
+    #results.update(location.dict())
+    #return results
+    return person
