bb5b63bfc07e3081fb5dc763c9da80fb7d40bb27
MichelleDuque <michelled_1994@hotmail.com>
2022-05-19 19:52:59 -0500

    deprecate_path_operation

--- main.py
+++ main.py
@@ -127,7 +127,8 @@ def create_person(person: Person = Body(...)):
     path="/person/detail",
     status_code=status.HTTP_200_OK,
     tags=["Persons"],
-    summary= "Get person details"
+    summary= "Get person details",
+    deprecated= True
     )
 def show_person(
     name: Optional[str] = Query(
